cc5100706328b0c3fc0fd4e4ba8fcdd0b2e7d5d2
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-09-18 21:11:34 -0300

    Add menu

---
+++ menu.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+submenu() {
+    echo -ne "
+SUBMENU
+1) SUBCMD1
+2) Go Back to Main Menu
+0) Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sub-submenu
+        submenu
+        ;;
+    2)
+        menu
+        ;;
+    0)
+        echo "Bye bye."
+        exit 0
+        ;;
+    *)
+        echo "Wrong option."
+        exit 1
+        ;;
+    esac
+}
+
+mainmenu() {
+    echo -ne "
+MAIN MENU
+1) CMD1
+0) Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        submenu
+        mainmenu
+        ;;
+    0)
+        echo "Bye bye."
+        exit 0
+        ;;
+    *)
+        echo "Wrong option."
+        exit 1
+        ;;
+    esac
+}
+
+mainmenu
--- termux/setup/setup-all.sh
+++ termux/setup/setup-all.sh
@@ -2,6 +2,19 @@
 
 start_time=$(date +%s)
 
+
+SETUP_TEMP_PATH=~/temp/setup
+arg1=foo
+arg2=bar
+
+function_name () {
+   echo "Parameter #1 is $1"
+}
+
+function_name "$arg1" "$arg2"
+
+mkdir -p $SETUP_TEMP_PATH
+
 end_time=$(date +%s)
 
 elapsed_time=$((end_time - start_time))
