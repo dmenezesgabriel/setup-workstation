972f366dee340a1d9855014a7e943b13548b6d9a
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-10-16 08:37:33 -0300

    feat: update termux setup script

--- termux/nix/default.nix
+++ termux/nix/default.nix
@@ -4,18 +4,25 @@ pkgs.mkShell {
   name = "termux-dev-env";
 
   buildInputs = [
-    pkgs.git
-    pkgs.vim
-    pkgs.htop
-    pkgs.nix
-    pkgs.zsh
-    pkgs.code-server
+    pkgs.git          # Git for version control
+    pkgs.vim          # Vim text editor
+    pkgs.htop         # Htop for system monitoring
+    pkgs.nix          # Nix package manager
+    pkgs.zsh          # Zsh shell
+    pkgs.code-server  # Code-server for remote VS Code
   ];
 
   shellHook = ''
-    sh -c "\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    git clone https://github.com/zsh-users/zsh-autosuggestions \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    # Install Oh My Zsh non-interactively
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+    # Install Zsh Autosuggestions plugin
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    # Update .zshrc to use the autosuggestions plugin
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
+
+    # Set Zsh as the default shell
     echo "exec zsh" > ~/.bashrc
   '';
 }
